4d71bba76e7a83f0dcaf0c919dad07dcae66a1c9
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-24 17:01:20 +0530

    Create RelationalOperaters.java

---
+++ RelationalOperaters.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int A = 10;
+        int B = 15;
+        System.out.println(A==B || A!=B || A>B || A<B || A>=B || A<=B);
+    }
+}
