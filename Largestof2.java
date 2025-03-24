1ac42669c3427d26d0b95c8d8689c73909edb63b
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-25 01:05:51 +0530

    Create Largestof2.java

---
+++ Largestof2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int A = 10;
+        int B = 5;
+        if(A >= B){
+            System.out.println("A is largest of 2");
+            System.out.print("A >= B");
+        
+        }else{
+            System.out.println("B is largest of 2");
+        }
+    }
+}
