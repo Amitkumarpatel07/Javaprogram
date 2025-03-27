b03403192b339ad6273e35be5bc027a9c25f5ff1
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-27 10:59:02 +0530

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int marks = 65;
+        
+        String reportCard = marks >= 33 ? "PASS" : "FAIL";
+        System.out.println(reportCard);
+    }
+}   
+        
