e56879c684f7e0673b8b591867e1a2e5dae5c6af
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-27 14:59:27 +0530

    Create FeverTemp.java

---
+++ FeverTemp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        double temp =101;
+        if(temp > 100){
+            System.out.println("You have a fever fever");
+    }
+    else{
+        System.out.println("You dont have a fever");
+        
+        }
+    }
+    
+}
