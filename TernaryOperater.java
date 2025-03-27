416be0a6b594d0f1027ca670a473dd3103972361
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-27 10:36:21 +0530

    Create TernaryOperater.java

---
+++ TernaryOperater.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int number = 55;
+        
+        String type = ((number%2) == 0) ? "even" : "odd";
+        System.out.println(type);
+    }
+}    
+        
