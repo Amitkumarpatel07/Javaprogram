1565017525806119ff1a73dcfbf511ee1c7e73f8
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-28 14:52:43 +0530

    Create Number1to100.java

---
+++ Number1to100.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String args []){
+        int counter = 1;
+        while(counter <=100){
+            System.out.println(counter+" " );
+            counter++;
+        }
+        System.out.println();
+    }
+}
