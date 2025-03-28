fd0497390b0574c0e7b97d3ce13597c2a7b1e14e
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-28 16:44:47 +0530

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String args []){
+       //for(int i=1; i<=3; i++){
+         // System.out.println("Hello world");
+       //}
+       int line = 1;
+       while(line <=4){
+           System.out.println("****");
+           line++;
+       }
+    }
+}
