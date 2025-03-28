60ee45c977d40e3958efc976e9b6b759961e6b50
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-28 22:09:18 +0530

    Create dowhileLoop.java

---
+++ dowhileLoop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int counter = 1;
+        do{
+            System.out.println("Hello world");
+            counter++;
+        }
+        while(counter <= 10);
+    }
+}
