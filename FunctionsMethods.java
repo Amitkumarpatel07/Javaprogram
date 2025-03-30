0ecd7b6fbc0ea0c52466d5ce6f16da385b6b2e08
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-31 00:10:13 +0530

    Create FunctionsMethods.java

---
+++ FunctionsMethods.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void printHelloWorld(){
+        System.out.println("Hello World ");
+        System.out.println("Hello world");
+        System.out.println("Hello World");
+    }
+    public static void main(String args []){
+        printHelloWorld();
+    }
+}
