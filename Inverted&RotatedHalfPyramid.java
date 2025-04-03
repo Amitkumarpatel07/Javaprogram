cd4c970829b6f6e4ec5d109d06abf15cd1b37fa5
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-04-03 21:45:39 +0530

    Create Inverted&RotatedHalfPyramid.java

---
+++ Inverted&RotatedHalfPyramid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void inverted_rotated_half_pyramid(int n){
+        for(int i=1; i<=n; i++){
+        
+          for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        } 
+        
+        System.out.println();
+    }
+}
+
+public static void main(String args[]){
+   // hollow_rectangle(10, 7);
+      inverted_rotated_half_pyramid(4);
+    }
+}    
